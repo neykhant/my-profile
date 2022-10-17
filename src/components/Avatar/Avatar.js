b043fcc0258bd2d568d6bed3b86b07bfc1a4e796
neykhant <khantmaw48@gmail.com>
2022-10-17 15:54:16 +0630

    done for experience not for project

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,23 +1,22 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core";
-import AvatarImg from "../../assets/images/avatar.jpg";
+// import AvatarImg from "../../assets/images/avatar.jpg";
+import AvatarImg from "../../assets/images/maw.png";
 
 const Avatar = () => {
   const classes = useStyles();
-  return (
-      <img src={AvatarImg} alt="Mehdi BHA" className={classes.avatarImg} />
-  );
+  return <img src={AvatarImg} alt="Mehdi BHA" className={classes.avatarImg} />;
 };
 
 const useStyles = makeStyles((theme) => ({
   avatarImg: {
-    borderRadius:'50%',
-    width:"270px",
-    height:"270px",
-    objectFit:'cover',
-    objectPosition: "0 -20px",
-    boxShadow:theme.shadows[10]
-  },
+    borderRadius: "50%",
+    width: "250px",
+    height: "250px",
+    objectFit: "cover",
+    // objectPosition: "0 -20px",
+    boxShadow: theme.shadows[10]
+  }
 }));
 
 export default Avatar;
