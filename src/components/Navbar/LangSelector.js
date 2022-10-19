45c8087e0daccc95e3c6760b33eb25e1eedd063f
neykhant <khantmaw48@gmail.com>
2022-10-19 09:52:31 +0630

    done for project galary

--- src/components/Navbar/LangSelector.js
+++ src/components/Navbar/LangSelector.js
@@ -6,91 +6,104 @@ import { useTranslation } from "react-i18next";
 import "flag-icon-css/css/flag-icon.min.css";
 
 const languages = [
-    {
-        code: "fr",
-        name: "FR",
-        country_code: "fr",
-    },
-    {
-        code: "en",
-        name: "EN",
-        country_code: "gb",
-    },
+  {
+    code: "mm",
+    name: "MM",
+    country_code: "mm"
+  },
+  {
+    code: "en",
+    name: "EN",
+    country_code: "gb"
+  }
 ];
 
 const LangSelector = (props) => {
-    const classes = useStyles();
-    const { t } = useTranslation();
-    const [anchorEl, setAnchorEl] = useState(null);
-    const handleClose = (code) => {
-        i18n.changeLanguage(code);
-        setAnchorEl(null);
-        if (props.onClose) {
-            props.onClose();
-        }
-    };
+  const classes = useStyles();
+  const { t } = useTranslation();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleClose = (code) => {
+    i18n.changeLanguage(code);
+    setAnchorEl(null);
+    if (props.onClose) {
+      props.onClose();
+    }
+  };
 
-    const currentLanguage = languages.find((elem) => elem.code === t("language"));
+  const currentLanguage = languages.find((elem) => elem.code === t("language"));
+//   console.log("ccl", currentLanguage);
 
-    return (
-        <div {...props}>
-            <Button variant="contained" color="primary" onClick={(e) => setAnchorEl(e.currentTarget)}>
-                {currentLanguage && (
-                    <>
-                        <span className={`flag-icon flag-icon-${currentLanguage.country_code} ${classes.flagIcon}`} />
-                        {currentLanguage.name}
-                    </>
-                )}
-                <KeyboardArrowDownSharp style={{ color: "white" }} />
-            </Button>
-            <Menu
-                id="profile-menu"
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={() => setAnchorEl(null)}
-                keepMounted
-                elevation={0}
-                getContentAnchorEl={null}
-                anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "center",
-                }}
-                transformOrigin={{
-                    vertical: "top",
-                    horizontal: "center",
-                }}
-                className={classes.profileMenu}
-                disableScrollLock
-            >
-                <MenuItem className={classes.menuItem} onClick={() => handleClose("en")}>
-                    <span className={`flag-icon flag-icon-gb ${classes.flagIcon}`} />
-                    EN
-                </MenuItem>
-                <MenuItem className={classes.menuItem} onClick={() => handleClose("fr")}>
-                    <span className={`flag-icon flag-icon-fr ${classes.flagIcon}`} />
-                    FR
-                </MenuItem>
-            </Menu>
-        </div>
-    );
+  return (
+    <div {...props}>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={(e) => setAnchorEl(e.currentTarget)}
+      >
+        {currentLanguage && (
+          <>
+            <span
+              className={`flag-icon flag-icon-${currentLanguage.country_code} ${classes.flagIcon}`}
+            />
+            {currentLanguage.name}
+          </>
+        )}
+        <KeyboardArrowDownSharp style={{ color: "white" }} />
+      </Button>
+      <Menu
+        id="profile-menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={() => setAnchorEl(null)}
+        keepMounted
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center"
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center"
+        }}
+        className={classes.profileMenu}
+        disableScrollLock
+      >
+        <MenuItem
+          className={classes.menuItem}
+          onClick={() => handleClose("en")}
+        >
+          <span className={`flag-icon flag-icon-gb ${classes.flagIcon}`} />
+          EN
+        </MenuItem>
+        <MenuItem
+          className={classes.menuItem}
+          onClick={() => handleClose("fr")}
+        >
+          <span className={`flag-icon flag-icon-mm ${classes.flagIcon}`} />
+          MM
+        </MenuItem>
+      </Menu>
+    </div>
+  );
 };
 
 const useStyles = makeStyles((theme) => ({
-    profileMenu: {
-        "& .MuiPaper-root": {
-            backgroundColor: theme.palette.primary.main,
-            marginTop: "2px",
-            boxShadow: theme.shadows[4],
-        },
-    },
-    menuItem: {
-        "&:hover": {
-            backgroundColor: theme.backgroundSecondary,
-        },
-    },
-    flagIcon: {
-        marginRight: theme.spacing(1),
-    },
+  profileMenu: {
+    "& .MuiPaper-root": {
+      backgroundColor: theme.palette.primary.main,
+      marginTop: "2px",
+      boxShadow: theme.shadows[4]
+    }
+  },
+  menuItem: {
+    "&:hover": {
+      backgroundColor: theme.backgroundSecondary
+    }
+  },
+  flagIcon: {
+    marginRight: theme.spacing(1)
+  }
 }));
 
 export default LangSelector;
--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ const skillsList = [
   { title: "React JS", value: 75 },
   { title: "Futter", value: 80 },
   { title: "Material UI", value: 80 },
-  { title: "Styled components", value: 80 },
+  { title: "Javascript", value: 80 },
   { title: "Laravel ", value: 60 },
   { title: "AntDesign ", value: 70 },
   { title: "Firebase", value: 70 }
@@ -10,24 +10,24 @@ const skillsList = [
 const projectList = [
   {
     id: 1,
-    title: "Tunflix",
-    technologies: ["React JS", "Node JS", "MongoDB"],
+    title: "Organic Shop Nail Admin Dashboard",
+    technologies: ["React JS", "Laravel", "Mysql", "Redux"],
     backgroundImage:
       "https://cdn.cbeditz.com/cbeditz/preview/black-red-gradient-background-wallpaper-74-11614352798fbqrv1wpuv.jpg",
     frontImage: "https://i.ibb.co/L0F7mKX/tunflix.png"
   },
   {
     id: 2,
-    title: "Code Hub",
-    technologies: ["React JS"],
+    title: "Galaxy Web App",
+    technologies: ["React JS", "Laravel", "Redux", "Mysql"],
     backgroundImage:
       "https://img.freepik.com/free-vector/dark-gradient-background-with-copy-space_53876-99548.jpg?size=626&ext=jpg&ga=GA1.2.2102900112.1628985600",
     frontImage: "https://i.ibb.co/zNMJFTW/Webp-net-resizeimage.png"
   },
   {
     id: 3,
-    title: "Burpees.io",
-    technologies: ["React JS", "Node JS", "MongoDB"],
+    title: "EPOS",
+    technologies: ["React JS", "Laravel", "Mysql", "Flutter"],
     backgroundImage:
       "https://media.istockphoto.com/vectors/abstract-purple-vector-background-with-stripes-vector-id972475894?k=6&m=972475894&s=612x612&w=0&h=99AirGMOb64N2-1ZSMYRjEBp2USrAdzXUGzQMh5o6Js=",
     frontImage:
@@ -38,18 +38,19 @@ const projectList = [
 const experienceList = [
   {
     id: 0,
-    company: "Code Hub",
+    company: "Real Code Solutions",
     links: {
       website: "https://rcs-mm.com/",
-      facebook: "https://www.facebook.com/codehubtn/",
-      instagram: "https://www.instagram.com/codehub.coworking/"
+      // facebook: "https://www.facebook.com/codehubtn/",
+      // instagram: "https://www.instagram.com/codehub.coworking/"
+      instagram: ""
     }
   },
   {
     id: 1,
-    company: "vapoter.tn",
+    company: "AndFund Yangon",
     links: {
-      instagram: "https://www.instagram.com/vapoter.tn/"
+      instagram: ""
     }
   }
 ];
